411a6e32346df7a594b646a02cfafa89a6eef039
lessless <me@lessless.pp.ua>
2013-05-06 17:14:57 +0300

    require singleton

--- app/cached.rb
+++ app/cached.rb
@@ -1,12 +1,16 @@
+# TODO: Query from database
+# TODO: Update info each 30m
+
+
 require_relative '../lib/nasa_adapter'
+require 'singleton'
 
 module Pease
   class Cached
-    # TODO: Query from database
-    # TODO: Update info each 30m
-
     include Singleton
+
     attr_accessor :astronauts
+
     def initialize
       @astronauts = NASA::ISS.new.current_astronauts
     end
